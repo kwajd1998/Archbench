e2b9b30638ad1e8b437c1ba33c9a747a1e8a7f62
Ruben Freitas <joseruben98@hotmail.com>
2020-04-28 11:56:32 +0100

    repo reset

--- ArchBench.PlugIns/IArchBenchSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ArchBench.PlugIns
-{
-    public interface IArchBenchSettings : IEnumerable<string>
-    {
-        string this[ string key] { get; set; }
-    }
-}
