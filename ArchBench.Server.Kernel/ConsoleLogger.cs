cd68faf14ba8e856a4bfbe4c295171a891254f80
Ruben Freitas <joseruben98@hotmail.com>
2020-04-28 11:56:58 +0100

    initial project

---
+++ ArchBench.Server.Kernel/ConsoleLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ArchBench.PlugIns;
+
+namespace ArchBench.Server.Kernel
+{
+    public class ConsoleLogger : IArchBenchLogger
+    {
+        public ConsoleLogger()
+        {
+            IndentLevel = 0;
+            IsNewLine = true;
+        }
+
+        public  int  IndentLevel { get; set; }
+        private bool IsNewLine   { get; set; }
+
+        public void Write( string aMessage )
+        {
+            if ( IsNewLine )
+            {
+                for (var i = 0; i < IndentLevel; i++)
+                    Console.Write("    ");
+                IsNewLine = false;
+            }
+            Console.Write( aMessage );
+        }
+
+        public void Write(string aFormat, params object[] aArgs)
+        {
+            Write(string.Format( aFormat, aArgs ) );
+        }
+
+
+        public void WriteLine()
+        {
+            Console.WriteLine();
+            IsNewLine = true;
+        }
+
+        public void WriteLine( string aFormat, params object[] aArgs )
+        {
+            WriteLine(string.Format( aFormat, aArgs ) );
+        }
+
+        public void WriteLine( string aMessage )
+        {
+            Write( aMessage );
+            WriteLine();
+        }
+    }
+}
